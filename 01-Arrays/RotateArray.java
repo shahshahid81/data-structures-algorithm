c9fbc08cf671b543db70cf35fe776354efb01359
Shahid Shah <shahshahid2123@gmail.com>
2023-04-30 15:10:07 +0530

    feat: added rotate array

---
+++ 01-Arrays/RotateArray.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+
+  private static void reverse(int[] array, int start, int end) {
+    while (start < end) {
+      int temp = array[start];
+      array[start] = array[end];
+      array[end] = temp;
+      start++;
+      end--;
+    }
+  }
+
+  public static void rotate(int[] array, int steps) {
+    reverse(array, 0, (steps % array.length) - 1);
+    reverse(array, (steps % array.length), array.length - 1);
+	reverse(array, 0, array.length - 1);
+  }
+
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = scanner.nextInt();
+    }
+    int steps = scanner.nextInt();
+    rotate(array, steps);
+    for (int element : array) {
+      System.out.print(element + " ");
+    }
+    scanner.close();
+  }
+}
---
+++ 01-Arrays/RotateArray.md
@@ -0,0 +1,32 @@
+# [Rotate Array](https://www.codingninjas.com/codestudio/problems/rotate-array_1230543)
+
+## Problem Statement
+Given an array with N elements  the task is to rotate the array to the left by K steps  where K is non-negative.
+
+___
+## Example
+Input: Given a sequence of five numbers 2  4  5  6  8.
+
+Output:  6
+
+Explanation:
+In the given sequence of numbers  number 8 is the largest element  followed by number 6 which is the second-largest element. Hence we return number 6 which is the second-largest element in the sequence.
+
+
+### Input / Output
+
+1.  
+    #### Input
+    7 5 2 11 2 43 1 1
+    2
+
+    #### Output
+    2 11 2 43 1 1 7 5
+
+2.  
+    #### Input
+    1 2 3 4 5 6 7
+    3
+
+    #### Output
+    5 6 7 1 2 3 4
