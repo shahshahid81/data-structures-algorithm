856ddb982ff29eeaa6d2b243db56f0485db91300
Shahid Shah <shahshahid2123@gmail.com>
2023-05-01 16:16:47 +0530

    feat: added code for equilibirium index

---
+++ 01-Arrays/Equilibirium.java
@@ -0,0 +1,24 @@
+public class Equilibirium {
+
+  public static int arrayEquilibriumIndex(int[] arr) {
+    if (arr.length == 0) {
+      return -1;
+    }
+
+    int total = 0;
+    for (int element : arr) {
+      total += element;
+    }
+
+    int leftSum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (total - leftSum - arr[i] == leftSum) {
+        return i;
+      }
+      leftSum += arr[i];
+    }
+
+    return -1;
+  }
+}
