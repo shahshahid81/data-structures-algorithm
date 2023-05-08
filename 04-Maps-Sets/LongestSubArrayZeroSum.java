a0bc02a437e7a11380715b0add709f39937b9c04
Shahid Shah <shahshahid2123@gmail.com>
2023-05-08 18:43:49 +0530

    feat: solved longest sub array zero sum

---
+++ 04-Maps-Sets/LongestSubArrayZeroSum.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LongestSubArrayZeroSum {
+
+  public static int LongestSubsetWithZeroSum(ArrayList<Integer> arr) {
+    int sum = 0, maxSubset = 0, n = arr.size();
+    HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+    for (int i = 0; i < n; i++) {
+      sum += arr.get(i);
+
+      if (sum == 0) maxSubset = i + 1;
+
+      Integer prev_i = hashMap.get(sum);
+
+      if (prev_i != null) {
+        maxSubset = Math.max(maxSubset, i - prev_i);
+      } else {
+        hashMap.put(sum, i);
+      }
+    }
+
+    return maxSubset;
+  }
+}
