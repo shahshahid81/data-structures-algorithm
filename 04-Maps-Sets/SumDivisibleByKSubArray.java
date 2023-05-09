e85e32beace2b5f53e25dc278e8d9760046e2a73
Shahid Shah <shahshahid2123@gmail.com>
2023-05-09 20:40:09 +0530

    feat: solved sum divisible by k sub array

---
+++ 04-Maps-Sets/SumDivisibleByKSubArray.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class SumDivisibleByKSubArray {
+
+  public static int subArrayCount(ArrayList<Integer> arr, int k) {
+    int[] map = new int[k];
+    map[0] = 1;
+    int count = 0, sum = 0;
+    for (int a : arr) {
+      sum = (sum + a) % k;
+      if (sum < 0) sum += k;
+      count += map[sum];
+      map[sum]++;
+    }
+    return count;
+  }
+}
