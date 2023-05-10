8c6ebd270fa4c32ccc162908085b32fbf1eaba48
Shahid Shah <shahshahid2123@gmail.com>
2023-05-11 00:43:56 +0530

    feat: solved is sub sequence

---
+++ 05-Two-Pointers/IsSubSequence.java
@@ -0,0 +1,23 @@
+public class IsSubSequence {
+
+  private static int indexOf(String str, char ch, int startIndex) {
+    int n = str.length();
+
+    for (int i = startIndex; i < n; i++) {
+      if (str.charAt(i) == ch) return i;
+    }
+    return -1;
+  }
+
+  public static String isSubsequence(String str1, String str2) {
+    int s1Length = str1.length(), s2Length = str2.length();
+    int i = 0, j = 0;
+    while (i < s1Length && j < s2Length) {
+      int index = indexOf(str2, str1.charAt(i), j);
+      if (index == -1) return "False";
+      i++;
+      j = index + 1;
+    }
+    return i >= s1Length ? "True" : "False";
+  }
+}
