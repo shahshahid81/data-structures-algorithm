d6ae1bcb93ff69a7437a5dc39b6b2b7f55ea01b5
Shahid Shah <shahshahid2123@gmail.com>
2023-05-11 23:13:11 +0530

    feat: solved max sub array

---
+++ 01-Arrays/MaxSubArray.java
@@ -0,0 +1,27 @@
+public class MaxSubArray {
+
+  // Leetcode
+  public int maxSubArray(int[] nums) {
+    int max = Integer.MIN_VALUE;
+    int currentIndex = 0;
+    int sum = 0;
+    while (currentIndex < nums.length) {
+      sum += nums[currentIndex];
+      max = Math.max(sum, max);
+      if (sum <= 0) sum = 0;
+      currentIndex++;
+    }
+    return max;
+  }
+
+  // Code Studio
+  public long maxSubarraySum(int[] arr, int n) {
+    int leftSum = 0, maxSum = 0;
+    for (int i = 0; i < n; i++) {
+      leftSum += arr[i];
+      if (leftSum < 0) leftSum = 0;
+      maxSum = Math.max(leftSum, maxSum);
+    }
+    return maxSum;
+  }
+}
