2d44f8b7ed5e6a26199d1aca2e72152c6a72535e
Shahid Shah <shahshahid2123@gmail.com>
2023-05-13 20:19:48 +0530

    feat: solved counting sort

---
+++ 06-Sorting/CountingSort.java
@@ -0,0 +1,30 @@
+public class CountingSort {
+
+  public static int[] sort(int arr[]) {
+    int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      min = Math.min(min, arr[i]);
+      max = Math.max(max, arr[i]);
+    }
+
+    int range = max - min + 1;
+
+    int[] count = new int[range];
+    int[] result = new int[arr.length];
+
+    for (int i = 0; i < arr.length; i++) {
+      count[arr[i] - min]++;
+    }
+
+    for (int i = 1; i < count.length; i++) {
+      count[i] += count[i - 1];
+    }
+
+    for (int i = arr.length - 1; i >= 0; i--) {
+      int index = --count[arr[i] - min];
+      result[index] = arr[i];
+    }
+
+    return result;
+  }
+}
--- 06-Sorting/README.md
+++ 06-Sorting/README.md
@@ -3,3 +3,4 @@
 | Insertion Sort | [Problem](https://www.codingninjas.com/codestudio/problems/insertion-sort_3155179) |          | [Solution](InsertionSort.java) |
 | Selection Sort | [Problem](https://www.codingninjas.com/codestudio/problems/selection-sort_981162)  |          | [Solution](SelectionSort.java) |
 | Bubble Sort    | [Problem](https://www.codingninjas.com/codestudio/problems/bubble-sort_980524)     |          | [Solution](BubbleSort.java)    |
+| Counting Sort  | [Problem](https://www.codingninjas.com/codestudio/problems/counting-sort_2663351)  |          | [Solution](CountingSort.java)  |
