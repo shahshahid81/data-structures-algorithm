621fbb58b717f39a682af83b855c7254131143a6
Shahid Shah <shahshahid2123@gmail.com>
2023-05-23 22:21:39 +0530

    feat: solved missing and repeating numbers

---
+++ 01-Arrays/MissingRepeatingNumber.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class MissingRepeatingNumber {
+
+  public static int[] missingAndRepeating(ArrayList<Integer> arr, int n) {
+    int repeatingElement = 0, missingElement = 0;
+    for (int i = 0; i < n; i++) {
+      int index = Math.abs(arr.get(i)) - 1;
+      if (arr.get(index) > 0) {
+        arr.set(index, -arr.get(index));
+      } else {
+        repeatingElement = index + 1;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (arr.get(i) > 0) {
+        missingElement = i + 1;
+      }
+    }
+
+    return new int[] { missingElement, repeatingElement };
+  }
+}
