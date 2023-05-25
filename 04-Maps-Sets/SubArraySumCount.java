1b3197bab56b11588945f1e23c795888bae9aea8
Shahid Shah <shahshahid2123@gmail.com>
2023-05-25 21:10:51 +0530

    feat: added leetcode question for sum of subarry equals k

--- 04-Maps-Sets/SubArraySumCount.java
+++ 04-Maps-Sets/SubArraySumCount.java
@@ -1,25 +1,17 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class SubArraySumCount {
-  public int findAllSubarraysWithGivenSum(int arr[], int s) {
-    if (arr == null || arr.length == 0) return 0;
-    int[] prefixSum = new int[arr.length];
-    int count = 0;
-    HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-    for (int i = 0; i < arr.length; i++) {
-      prefixSum[i] = arr[i] + (i > 0 ? prefixSum[i - 1] : 0);
-      if (prefixSum[i] == s) count++;
-      hashMap.put(prefixSum[i], hashMap.getOrDefault(prefixSum[i], 0) + 1);
+  public int subarraySum(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int sum = 0, count = 0;
+    for (int num : nums) {
+      sum += num;
+      if (sum == k) count++;
+      if (map.containsKey(sum - k)) count += map.get(sum - k);
+      map.put(sum, map.getOrDefault(sum, 0) + 1);
     }
-
-    for (int i = prefixSum.length - 1; i >= 0; i--) {
-      int target = prefixSum[i] - s;
-      if (hashMap.containsKey(target)) {
-        count = count + hashMap.get(target);
-      }
-    }
-
     return count;
   }
 }
