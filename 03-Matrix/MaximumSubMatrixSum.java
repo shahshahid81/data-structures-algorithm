d5f18d8c93bda0be787d49c52eb3df20de38a3ea
Shahid Shah <shahshahid2123@gmail.com>
2023-05-27 00:04:50 +0530

    feat: solved max sub matrix sum

---
+++ 03-Matrix/MaximumSubMatrixSum.java
@@ -0,0 +1,27 @@
+public class MaximumSubMatrixSum {
+
+  private static int getMaxSum(int[] arr) {
+    int maxSum = Integer.MIN_VALUE, currentSum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      currentSum += arr[i];
+      maxSum = Math.max(currentSum, maxSum);
+      if (currentSum < 0) currentSum = 0;
+    }
+    return maxSum;
+  }
+
+  public static int maxSumRectangle(int[][] arr, int rows, int columns) {
+    int maxSum = Integer.MIN_VALUE;
+    for (int i = 0; i < columns; i++) {
+      int[] sumArray = new int[rows];
+      for (int j = i; j < columns; j++) {
+        for (int k = 0; k < rows; k++) {
+          sumArray[k] += arr[k][j];
+        }
+        maxSum = Math.max(maxSum, getMaxSum(sumArray));
+      }
+    }
+
+    return maxSum;
+  }
+}
