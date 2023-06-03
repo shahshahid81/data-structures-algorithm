e7f0a461cb6907d7289ff3b5900ea313b0bf24e7
Shahid Shah <shahshahid2123@gmail.com>
2023-06-03 16:39:46 +0530

    feat: solved row wise sorted matrix median

---
+++ 07-Binary-Search/RowWiseSortedMatrixMedian.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RowWiseSortedMatrixMedian {
+
+  public static int getMedian(ArrayList<ArrayList<Integer>> matrix) {
+    int rows = matrix.size(), columns = matrix.get(0).size();
+
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+
+    for (int i = 0; i < rows; i++) {
+      if (matrix.get(i).get(0) < min) {
+        min = matrix.get(i).get(0);
+      }
+
+      if (matrix.get(i).get(columns - 1) > max) {
+        max = matrix.get(i).get(columns - 1);
+      }
+    }
+
+    int desired = (rows * columns + 1) / 2;
+    while (min < max) {
+      int mid = min + (max - min) / 2;
+      int place = 0;
+      int get = 0;
+
+      for (int i = 0; i < rows; ++i) {
+        get =
+          Arrays.binarySearch(
+            matrix.get(i).stream().mapToInt(val -> val).toArray(),
+            mid
+          );
+
+        if (get < 0) {
+          get = Math.abs(get) - 1;
+        } else {
+          while (get < matrix.get(i).size() && matrix.get(i).get(get) == mid) {
+            get += 1;
+          }
+        }
+
+        place = place + get;
+      }
+
+      if (place < desired) {
+        min = mid + 1;
+      } else {
+        max = mid;
+      }
+    }
+    return min;
+  }
+}
