1d2de1eea2d670593c023cdb0c5bf9c48cfc99a5
Shahid Shah <shahshahid2123@gmail.com>
2023-06-05 22:58:00 +0530

    feat: solved merge sort

---
+++ 09-Recursion/MergeSort.java
@@ -0,0 +1,38 @@
+public class MergeSort {
+
+  private static int[] merge(int[] arr1, int[] arr2) {
+    int i = 0, j = 0, k = 0;
+    int[] result = new int[arr1.length + arr2.length];
+
+    while (i < arr1.length && j < arr2.length) {
+      result[k++] = arr1[i] <= arr2[j] ? arr1[i++] : arr2[j++];
+    }
+
+    while (i < arr1.length) {
+      result[k++] = arr1[i++];
+    }
+
+    while (j < arr2.length) {
+      result[k++] = arr2[j++];
+    }
+
+    return result;
+  }
+
+  private static int[] mergeSort(int[] arr, int low, int high) {
+    if (low == high) {
+      return new int[] { arr[low] };
+    }
+
+    int mid = low + (high - low) / 2;
+    return merge(mergeSort(arr, low, mid), mergeSort(arr, mid + 1, high));
+  }
+
+  public static void mergeSort(int[] arr, int n) {
+    int low = 0, high = arr.length - 1;
+    int[] result = mergeSort(arr, low, high);
+    for (int i = 0; i < n; i++) {
+      arr[i] = result[i];
+    }
+  }
+}
---
+++ 09-Recursion/README.md
@@ -0,0 +1,4 @@
+| Question   | Code Studio                                                                   | Leetcode | Solution                   |
+| ---------- | ----------------------------------------------------------------------------- | -------- | -------------------------- |
+| Merge Sort | [Problem](https://www.codingninjas.com/codestudio/problems/merge-sort_920442) |          | [Solution](MergeSort.java) |
+
