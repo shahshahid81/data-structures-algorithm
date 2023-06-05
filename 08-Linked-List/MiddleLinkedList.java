b844d528fdab0e3f132d8b4a3677af2e11e446db
Shahid Shah <shahshahid2123@gmail.com>
2023-06-05 22:28:46 +0530

    feat: solved middle of linked list

---
+++ 08-Linked-List/MiddleLinkedList.java
@@ -0,0 +1,38 @@
+class Node {
+
+  public int data;
+  public Node next;
+
+  Node() {
+    this.data = 0;
+    this.next = null;
+  }
+
+  Node(int data) {
+    this.data = data;
+    this.next = null;
+  }
+
+  Node(int data, Node next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+public class MiddleLinkedList {
+
+  public static Node findMiddle(Node head) {
+    Node slow = head, fast = head;
+
+    while (fast != null && fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    if (fast.next != null) {
+      slow = slow.next;
+    }
+
+    return slow;
+  }
+}
