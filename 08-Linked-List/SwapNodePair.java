b68febdb20e60b36eac05a99abd73fa5b25da984
Shahid Shah <shahshahid2123@gmail.com>
2023-06-14 00:32:32 +0530

    feat: solved separate node pair

---
+++ 08-Linked-List/SwapNodePair.java
@@ -0,0 +1,34 @@
+class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+
+  public ListNode swapPairs(ListNode head) {
+    ListNode dummy = new ListNode(-1, head);
+    ListNode prev = dummy, current = head;
+    while (current != null && current.next != null) {
+      ListNode next = current.next;
+      ListNode temp = next.next;
+      next.next = current;
+      current.next = temp;
+      prev.next = next;
+      prev = current;
+      current = temp;
+    }
+    return dummy.next;
+  }
+}
