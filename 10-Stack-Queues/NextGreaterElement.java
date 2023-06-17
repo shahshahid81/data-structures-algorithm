fc112f6560ee71e58bd23feaee4c81424a17afe1
Shahid Shah <shahshahid2123@gmail.com>
2023-06-17 17:16:21 +0530

    feat: solved next greater element

---
+++ 10-Stack-Queues/NextGreaterElement.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class NextGreaterElement {
+
+  public static int[] nextGreater(int[] arr, int n) {
+    int[] result = new int[n];
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = n - 1; i >= 0; i--) {
+      while (!stack.isEmpty()) {
+        if (stack.peek() > arr[i]) {
+          result[i] = stack.peek();
+          stack.push(arr[i]);
+          break;
+        } else {
+          stack.pop();
+        }
+      }
+
+      if (stack.isEmpty()) {
+        result[i] = -1;
+        stack.push(arr[i]);
+      }
+    }
+
+    return result;
+  }
+}
