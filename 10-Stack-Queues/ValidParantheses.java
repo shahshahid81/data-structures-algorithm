06e723964f7ce70ef9684141157f3d694414e35e
Shahid Shah <shahshahid2123@gmail.com>
2023-06-17 15:35:34 +0530

    feat: solved valid parantheses

---
+++ 10-Stack-Queues/ValidParantheses.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char ch : s.toCharArray()) {
+      if (ch == '[' || ch == '(' || ch == '{') {
+        stack.add(ch);
+      } else {
+        if (ch == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+          return false;
+        } else if (ch == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+          return false;
+        } else if (ch == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+}
