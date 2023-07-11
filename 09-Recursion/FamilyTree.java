18ef2160544ab4f3ba6fae3a885fb5b895c3854b
Shahid Shah <shahshahid2123@gmail.com>
2023-07-12 01:16:48 +0530

    feat: refactored family tree

--- 09-Recursion/FamilyTree.java
+++ 09-Recursion/FamilyTree.java
@@ -1,15 +1,17 @@
 public class FamilyTree {
 
   public String kthChildNthGeneration(int n, long k) {
-    if (n == 1 && k == 1) {
+    if (n == 1) {
       return "Male";
     }
 
-    long parent = (long) Math.floor((k + 1) / 2);
-    String parentGender = kthChildNthGeneration(n - 1, parent);
+    String parent = kthChildNthGeneration(n - 1, (k + 1) / 2);
+
     boolean isFirstChild = k % 2 != 0;
-    return isFirstChild
-      ? parentGender.equals("Male") ? "Male" : "Female"
-      : parentGender.equals("Male") ? "Female" : "Male";
+    if (parent.equals("Male")) {
+      return isFirstChild ? "Male" : "Female";
+    }
+
+    return isFirstChild ? "Female" : "Male";
   }
 }
