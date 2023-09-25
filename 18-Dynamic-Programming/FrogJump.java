68dafdf8f9ba78992e308bace3e0aaf1e094da6c
Shahid Shah <shahshahid2123@gmail.com>
2023-09-25 16:54:53 +0530

    feat: solved frog jump

---
+++ 18-Dynamic-Programming/FrogJump.java
@@ -0,0 +1,126 @@
+import java.util.Arrays;
+
+// https://takeuforward.org/data-structure/dynamic-programming-frog-jump-dp-3/
+
+class FrogJumpRecursion {
+
+  public static int frogJump(int n, int heights[]) {
+    return frogJump(n, heights, 0);
+  }
+
+  private static int frogJump(int n, int heights[], int currentStep) {
+    if (currentStep >= n || currentStep + 1 >= n) {
+      return 0;
+    }
+
+    int oneStepEnergy =
+      Math.abs(heights[currentStep + 1] - heights[currentStep]) +
+      frogJump(n, heights, currentStep + 1);
+
+    int twoStepEnergy = Integer.MAX_VALUE;
+    if (currentStep + 2 < n) {
+      twoStepEnergy =
+        Math.abs(heights[currentStep + 2] - heights[currentStep]) +
+        frogJump(n, heights, currentStep + 2);
+    }
+
+    return Math.min(oneStepEnergy, twoStepEnergy);
+  }
+}
+
+class FrogJumpRecursionMemoization {
+
+  public static int frogJump(int n, int heights[]) {
+    // Memoized Code Start
+    int[] dp = new int[n];
+    Arrays.fill(dp, -1);
+    // Memoized Code End
+
+    return frogJump(
+      n,
+      heights,
+      0,
+      // Memoized Code Start
+      dp
+      // Memoized Code End
+    );
+  }
+
+  private static int frogJump(int n, int heights[], int currentStep, int[] dp) {
+    if (currentStep >= n || currentStep + 1 >= n) {
+      return 0;
+    }
+
+    // Memoized Code Start
+    if (dp[currentStep] != -1) {
+      return dp[currentStep];
+    }
+    // Memoized Code End
+
+    int oneStepEnergy =
+      Math.abs(heights[currentStep + 1] - heights[currentStep]) +
+      frogJump(n, heights, currentStep + 1, dp);
+
+    int twoStepEnergy = Integer.MAX_VALUE;
+    if (currentStep + 2 < n) {
+      twoStepEnergy =
+        Math.abs(heights[currentStep + 2] - heights[currentStep]) +
+        frogJump(n, heights, currentStep + 2, dp);
+    }
+
+    // Memoized Code Start
+    dp[currentStep] = Math.min(oneStepEnergy, twoStepEnergy);
+    // Memoized Code End
+
+    return dp[currentStep];
+  }
+}
+
+class FrogJumpTabulation {
+
+  public static int frogJump(int n, int heights[]) {
+    int[] dp = new int[n];
+
+    for (int currentStep = 1; currentStep < n; currentStep++) {
+      int oneStepEnergy =
+        Math.abs(heights[currentStep] - heights[currentStep - 1]) +
+        dp[currentStep - 1];
+
+      int twoStepEnergy = Integer.MAX_VALUE;
+      if (currentStep > 1) {
+        twoStepEnergy =
+          Math.abs(heights[currentStep] - heights[currentStep - 2]) +
+          dp[currentStep - 2];
+      }
+
+      dp[currentStep] = Math.min(oneStepEnergy, twoStepEnergy);
+    }
+
+    return dp[n - 1];
+  }
+}
+
+class FrogJumpTabulationSpaceOptimized {
+
+  public static int frogJump(int n, int heights[]) {
+    int previousOneStep = 0, previousTwoStep = 0;
+
+    for (int currentStep = 1; currentStep < n; currentStep++) {
+      int oneStepEnergy =
+        Math.abs(heights[currentStep] - heights[currentStep - 1]) +
+        previousOneStep;
+
+      int twoStepEnergy = Integer.MAX_VALUE;
+      if (currentStep > 1) {
+        twoStepEnergy =
+          Math.abs(heights[currentStep] - heights[currentStep - 2]) +
+          previousTwoStep;
+      }
+
+      previousTwoStep = previousOneStep;
+      previousOneStep = Math.min(oneStepEnergy, twoStepEnergy);
+    }
+
+    return previousOneStep;
+  }
+}
--- 18-Dynamic-Programming/README.md
+++ 18-Dynamic-Programming/README.md
@@ -1,6 +1,8 @@
 | Question                 | Code Studio                                                                                   | Leetcode                                                 | Solution                             |
 | ------------------------ | --------------------------------------------------------------------------------------------- | -------------------------------------------------------- | ------------------------------------ |
 | Climbing Stairs          | [Problem](https://www.codingninjas.com/studio/problems/count-ways-to-reach-nth-stairs_798650) | [Problem](https://leetcode.com/problems/climbing-stairs) | [Solution](ClimbStairs.java)         |
+| Frog Jump                | [Problem](https://www.codingninjas.com/studio/problems/frog-jump_3621012)                     |                                                          | [Solution](FrogJump.java)            |
 | House Robber             | [Problem](https://www.codingninjas.com/studio/problems/loot-houses_630510)                    | [Problem](https://leetcode.com/problems/house-robber)    | [Solution](HouseRobber.java)         |
 | Ways To Make Coin Change | [Problem](https://www.codingninjas.com/studio/problems/630471)                                | [Problem](https://leetcode.com/problems/coin-change-ii)  | [Solution](TotalWaysCoinChange.java) |
 | Rod Cutting              | [Problem](https://www.codingninjas.com/studio/problems/rod-cutting-problem_800284)            |                                                          | [Solution](RodCutting.java)          |
+
